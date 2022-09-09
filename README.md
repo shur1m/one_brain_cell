f298758cb29bdcb890e45a188c360a7d5af2acb4
Shurim <ansondoeswork@gmail.com>
2022-09-09 15:12:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# one_brain_cell
+ A flashcard for your one brain cell.
---
+++ android/app/src/main/kotlin/com/example/one_brain_cell/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.one_brain_cell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
