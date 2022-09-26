637fe715cae2a655bf76e943c8e49f5706211be3
Shurim <ansondoeswork@gmail.com>
2022-09-25 22:56:07 -0700

    idlistbox bug fixes

--- lib/utils/store/cardCollection.dart
+++ lib/utils/store/cardCollection.dart
@@ -22,23 +22,26 @@ class CardCollection extends HiveObject {
   @override
   Future<void> delete() {
     //call delete for all children
-    _deleteContents();
+    _deleteContents(key);
     return super.delete();
   }
 
-  void _deleteContents() async {
+  void _deleteContents(int collectionKey) async {
     //if this collection is list, we need to delete
     //everything inside it as well as self from idlist box
     if (isList) {
       Box idListBox = Hive.box('idlists');
-      List<int> idList = idListBox.get(key);
+      List<int> idList = idListBox.get(collectionKey);
 
       //delete all cards inside from database
       Database db = await SqlHelper.getDatabase('flashcards.db');
       db.execute('DELETE FROM Flashcards WHERE ID IN (${idList.join(', ')})');
 
       //delete idlists from box
-      idListBox.delete(collectionName);
+      idListBox.delete(collectionKey);
+      print(collectionKey);
+      print(idListBox.toMap());
+      print(await db.rawQuery('SELECT * FROM Flashcards'));
       return;
     }
 
